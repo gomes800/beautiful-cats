32f6f1d627718db3ce14461f2b58e5172f28739a
gomes <joaogomesleite800@gmail.com>
2025-05-13 09:46:00 -0300

    first commit

---
+++ backend/src/main/java/com/gom/beautiful_cats/BeautifulCatsApplication.java
@@ -0,0 +1,13 @@
+package com.gom.beautiful_cats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautifulCatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautifulCatsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gom/beautiful_cats/BeautifulCatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gom.beautiful_cats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautifulCatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
